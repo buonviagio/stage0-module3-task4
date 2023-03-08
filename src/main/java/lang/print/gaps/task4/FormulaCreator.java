5eb1aa26ae5b3ef6be834b6bca63b00362d8ee42
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-08 19:30:21 +0200

    My fisrt commit Module3 Task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 1.0f;
+        float b = 1.0f;
+        double result;
+        result = ((9 * (a * a)) - (5 * b) + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int cInt = 99, hInt = 104, aInt = 97, rInt = 114;
+        char c = (char)cInt;
+        char h = (char)hInt;
+        char a = (char)aInt;
+        char r = (char)rInt;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
